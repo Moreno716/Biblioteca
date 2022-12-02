157c79bf03b2509c709046ce4f9eebe7e29fe693
Juan Diego Moreno <juandiegomc00@gmail.com>
2022-12-02 16:04:51 -0500

    consulta base de datos e instalacion bootstrap

---
+++ app/Views/libros/listar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    Lista de libros
+    <?php print_r($libros); ?>  
+    <div class="container">
+        <table class="table table-light">
+            <thead class="thead-light">
+                <tr>
+                    <th>#</th>
+                    <th>Imagen</th>
+                    <th>Nombre</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Imagen</td>
+                    <td>Nombre</td>
+                    <td>Editar/Borrar</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
